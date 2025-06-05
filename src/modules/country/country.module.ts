70b81d454e2d03caf0f705d861d8e0666b631d07
Pushkal-godara <godarapushkal@gmail.com>
2025-06-06 00:12:06 +0530

    fixing dependency issues

--- src/modules/country/country.module.ts
+++ src/modules/country/country.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CountryService } from './country.service';
+import { UserModule } from '../user/user.module';
 import { CountryController } from './country.controller';
 import { Country } from './entities/country.entity';
 import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-    imports: [SequelizeModule.forFeature([Country])],
+    imports: [SequelizeModule.forFeature([Country]), UserModule],
     controllers: [CountryController],
     providers: [CountryService],
 })
